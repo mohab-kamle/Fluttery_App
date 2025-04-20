5482d2bf96da6ac33bea723333a5d35fa90cce80
Mohab <mohabkamle9@gmail.com>
2025-04-20 10:32:36 +0200

    Add: Integrate Firebase and restructure project files

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_at_akira_menai/homePage.dart';
+import 'package:flutter_at_akira_menai/home_page.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
-
+import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  //initialize firebase
+  await Firebase.initializeApp(
+   options: DefaultFirebaseOptions.currentPlatform,
+  );
   //initilize Hivebox
   final dir = await getApplicationDocumentsDirectory(); // get directory path
   Hive.init(dir.path); // Initialize Hive with directory
